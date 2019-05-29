34a824db6cbe1748c66a4a5faac20a9e272a971b
Matt Rose <matthew.rose@maxkelsen.com>
2019-05-29 11:18:41 +1000

    rebuilt docs

--- gym_quantcircuit/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='quantcircuit-v0',
-    entry_point='gym_quantcircuit.envs:QuantCircuitEnv'
-)
--- gym_quantcircuit/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_quantcircuit.envs.QuantCircuitEnv import QuantCircuitEnv
